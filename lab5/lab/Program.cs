386653192dac24184d72d77d92958836ec49a4aa
SavchukValeria <V.Savchuk_FIT_12_21_B_d@knute.edu.ua>
2022-11-18 16:04:16 +0200

    ll
    ll

---
+++ lab5/lab/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab
+{
+    internal class MyClass
+    {
+        public static T Factory<T>() where T : new() => new();
+    }
+}
